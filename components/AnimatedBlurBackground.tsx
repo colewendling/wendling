63c41f18c41ffa3a639d2bb9843645d2b432e742
Cole Wendling <cole@wendling.io>
2025-04-13 15:07:28 +0200

    refactor AnimatedBlurBackground for interactivity

--- components/AnimatedBlurBackground.tsx
+++ components/AnimatedBlurBackground.tsx
@@ -1,18 +1,72 @@
-import Image from "next/image";
-import backgroundImage from "@/public/images/general/background.jpg";
+// components/AnimatedBlurBackground.tsx
 
-export const AnimatedBlurBackground = () => {
+"use client";
+
+import React, { useEffect, useRef } from "react";
+
+
+const AnimatedBlurBackground: React.FC = () => {
+  const interactiveRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let curX = 0,
+      curY = 0,
+      tgX = 0,
+      tgY = 0;
+    const el = interactiveRef.current!;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      el.style.transform = `translate(${Math.round(curX)}px, ${Math.round(
+        curY
+      )}px)`;
+      requestAnimationFrame(move);
+    }
+
+    const handleMouseMove = (e: MouseEvent) => {
+      tgX = e.clientX;
+      tgY = e.clientY;
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+
+    move();
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="fixed inset-0 overflow-hidden">
-      <Image
-        src={backgroundImage}
-        alt="Animated Background"
-        fill
-        priority
-        placeholder="blur"
-        className="object-cover opacity-80 animated-background"
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-30" />
+    <div className="gradient-bg fixed inset-0 z-0 overflow-hidden pointer-events-none">
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="20"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -10"
+              result="goo"
+            />
+            <feBlend in="SourceGraphic" in2="goo" />
+          </filter>
+        </defs>
+      </svg>
+
+      <div className="gradients-container">
+        <div className="g1" />
+        <div className="g2" />
+        <div className="g3" />
+        <div className="g4" />
+        <div className="g5" />
+        <div ref={interactiveRef} className="interactive" />
+      </div>
     </div>
   );
 };
+
+export default AnimatedBlurBackground;
