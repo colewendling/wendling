f31c62c6c6aea93672f24c771f12ddef62a76010
Cole Wendling <cole@wendling.io>
2025-04-16 18:24:18 +0200

    add HomeBackground wrapper comp. to use in root layout.tsx

---
+++ components/HomeBackground.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import AnimatedBlurBackground from "./AnimatedBlurBackground";
+
+export default function HomeBackground() {
+  const pathname = usePathname();
+  return pathname === "/" ? <AnimatedBlurBackground /> : null;
+}
