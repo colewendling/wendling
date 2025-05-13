378bff9ee5b78a6826662b09e1a331d67f84844d
Cole Wendling <cole@wendling.io>
2025-05-13 09:57:18 +0200

    adjust section padding in mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Section variant="animated">
         <ProfileCard />
       </Section>
-      <h1 className="text-xl tracking-widest text-white text-right px-20 opacity-70">
+      <h1 className="text-xl tracking-widest text-white text-right px-10 md:px-20 opacity-70">
         Projects
       </h1>
       <Section variant="solid">
